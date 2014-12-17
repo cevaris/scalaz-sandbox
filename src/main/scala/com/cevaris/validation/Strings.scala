41b1787c91e786842e3b3e70cafe611b4a6f9438
Adam Cardenas <cevaris@gmail.com>
2014-12-17 15:16:55 -0700

    converted parse list func to a partial function

--- src/main/scala/com/cevaris/validation/Strings.scala
+++ src/main/scala/com/cevaris/validation/Strings.scala
@@ -2,6 +2,8 @@ package com.cevaris.validation
 
 import scalaz._, Scalaz._
 import scalaz.Validation
+import scalaz.stream._
+import scalaz.concurrent._
 
 object Strings {
 
@@ -9,13 +11,16 @@ object Strings {
   case class InvalidList(s: String)
   case class NumberNotEven(s: String) extends NumError
 
+  val zeroTo100i: Process[Task, Int] = Process.range(0,100)
+  
   def splitString(s: String): List[String] = s.split(" ").toList
 
-  def parseList[A](s: String, parser: (String => A)): ValidationNel[String, List[A]] = {
-    val splitted = splitString(s)
-    splitted.map(parser).successNel[String]
-  }
+  def parseList[A](parser: (String => A))(s: String): ValidationNel[NumError, List[A]] =
+    splitString(s).map(parser).successNel[NumError]
 
+  // def parseList[A](s: String, parser: (String => A)): ValidationNel[NumError, List[A]] =
+  //   splitString(s).map(parser).successNel[NumError]
+    
   def parseVal(s: String): Option[Int] = parseInt(s) match {
     case Success(a) => Some(a)
     case Failure(e) => None
