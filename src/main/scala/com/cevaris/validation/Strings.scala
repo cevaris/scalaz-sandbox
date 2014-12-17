d79028629d43c7c5fe1ab527eaef03ef95768b22
Adam Cardenas <cevaris@gmail.com>
2014-12-17 14:53:23 -0700

    Eh, got working simple case

--- src/main/scala/com/cevaris/validation/Strings.scala
+++ src/main/scala/com/cevaris/validation/Strings.scala
@@ -1,11 +1,20 @@
 package com.cevaris.validation
 
 import scalaz._, Scalaz._
+import scalaz.Validation
 
 object Strings {
 
   sealed trait NumError
+  case class InvalidList(s: String)
   case class NumberNotEven(s: String) extends NumError
+
+  def splitString(s: String): List[String] = s.split(" ").toList
+
+  def parseList[A](s: String, parser: (String => A)): ValidationNel[String, List[A]] = {
+    val splitted = splitString(s)
+    splitted.map(parser).successNel[String]
+  }
 
   def parseVal(s: String): Option[Int] = parseInt(s) match {
     case Success(a) => Some(a)
